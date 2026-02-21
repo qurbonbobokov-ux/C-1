e76a1989bd28f70f4e323047e417aa93afb89ee2
Qurbonbek <qurbonbobokov@gmail.com>
2026-02-21 16:49:14 +0500

    sect

--- obj/Debug/net10.0/Hometask.AssemblyInfo.cs
+++ obj/Debug/net10.0/Hometask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hometask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758d4c31fa1d583bece3a7b5ccd35128bcaacfdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hometask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hometask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
